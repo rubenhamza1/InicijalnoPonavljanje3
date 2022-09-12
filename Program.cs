10bf31771b437e22d13a4e6197416b3d87d7d438
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-09-12 12:40:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+
+            a = (int)Convert.ToDouble(a);
+            b = (int)Convert.ToDouble(b);
+            Console.WriteLine("Prosječna vrijednost: " + (a + b + c + d) / 4);
+            Console.ReadKey();
+        }
+    }
+}
